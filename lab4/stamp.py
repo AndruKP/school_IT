7d2e45d4e267545e91141f45510301308acd706c
AndruKP <andruhovski@outlook.com>
2021-04-21 11:37:01 +0300

    Added squares

--- lab4/stamp.py
+++ lab4/stamp.py
@@ -7,15 +7,35 @@ t.pd()
 t.st()
 
 
-def move():
+def turtle():
     t.lt(90)
     t.shape('turtle')
     t.color('yellow', 'green')
 
     for i in range(1, 4):
-        t.shapesize(9-2*i, 9 - 2*i, 2)
+        t.shapesize(9 - 2 * i, 9 - 2 * i, 2)
         t.stamp()
 
 
-move()
+def squares():
+    t.fd(100)
+    t.shape('square')
+    t.shapesize(6, 6, 2)
+    t.color('red', 'teal')
+    t.stamp()
+
+    t.shape('circle')
+    t.color('red', 'white')
+    t.stamp()
+
+    t.shape('square')
+    t.shapesize(4, 4, 2)
+    t.color('red', 'teal')
+    t.stamp()
+
+
+turtle()
+t.pu()
+t.fd(100)
+squares()
 t.exitonclick()
