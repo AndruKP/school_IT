485f0d060b8b3d9ee4c1e147bf623cd564e36001
AndruKP <andruhovski@outlook.com>
2021-04-21 12:20:42 +0300

    Added magic drawing

--- lab4/stamp.py
+++ lab4/stamp.py
@@ -3,12 +3,11 @@ import turtle as t
 t.reset()
 t.title('Петухон - лучший язык программирования')
 
-t.pd()
-t.st()
+t.pu()
+t.ht()
 
 
 def turtle():
-    t.lt(90)
     t.shape('turtle')
     t.color('yellow', 'green')
 
@@ -18,7 +17,6 @@ def turtle():
 
 
 def squares():
-    t.fd(100)
     t.shape('square')
     t.shapesize(6, 6, 2)
     t.color('red', 'teal')
@@ -34,8 +32,73 @@ def squares():
     t.stamp()
 
 
+def triangles():
+    #t.lt(90)
+    t.shape('triangle')
+    t.color('blue', 'light blue')
+    for i in range(5):
+        t.shapesize(6 - i, 6 - i, 2)
+        t.stamp()
+
+
+def arrows():
+    t.lt(90)
+    t.shape('arrow')
+    t.color('red', 'misty rose')
+    for i in range(5):
+        t.shapesize(6 - i, 6 - i, 2)
+        t.stamp()
+
+
+def square():
+    t.shape('square')
+    t.color('green', 'light green')
+    for i in range(3):
+        t.shapesize(6 - 2 * i, 6 - 2 * i, 2)
+        t.stamp()
+
+
+def rainbow():
+    t.shape('circle')
+
+    t.color('red')
+    t.shapesize(7, 7, 2)
+    t.stamp()
+
+    t.color('orange')
+    t.shapesize(6, 6, 2)
+    t.stamp()
+
+    t.color('yellow')
+    t.shapesize(5, 5, 2)
+    t.stamp()
+
+    t.color('green')
+    t.shapesize(4, 4, 2)
+    t.stamp()
+
+    t.color('blue')
+    t.shapesize(3, 3, 2)
+    t.stamp()
+
+    t.color('dark blue')
+    t.shapesize(2, 2, 2)
+    t.stamp()
+
+    t.color('purple')
+    t.shapesize(1, 1, 2)
+    t.stamp()
+
+
+rainbow()
+t.setpos(-300,0)
+square()
+t.setpos(0,200)
+arrows()
+t.setpos(-150,200)
+triangles()
+t.setpos(-150,0)
 turtle()
-t.pu()
-t.fd(100)
+t.setpos(-300,200)
 squares()
 t.exitonclick()
